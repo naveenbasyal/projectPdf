8678977ba61412335aeec7a952755973a9f5213d
Naveen Basyal <naveenbasyal.001@gmail.com>
2023-02-20 23:04:26 +0530

    Done : user can choose the page range

--- src/components/ExtractImages.jsx
+++ src/components/ExtractImages.jsx
@@ -3,6 +3,8 @@ import { PDFDocument } from "pdf-lib";
 
 export default function Home() {
   const [pdfFileData, setPdfFileData] = useState();
+  const [pdfPagesCount, setPdfPagesCount] = useState();
+  const [extractedPdfFileData, setExtractedPdfFileData] = useState();
 
   function readFileAsync(file) {
     return new Promise((resolve, reject) => {
@@ -15,54 +17,81 @@ export default function Home() {
     });
   }
 
-  function renderPdf(uint8array) {
+  async function renderPdf(uint8array) {
     const tempblob = new Blob([uint8array], {
       type: "application/pdf",
     });
     const docUrl = URL.createObjectURL(tempblob);
     setPdfFileData(docUrl);
+
+    const pdfDoc = await PDFDocument.load(uint8array);
+    setPdfPagesCount(pdfDoc.getPageCount());
   }
 
-  function range(start, end) {
-    let length = end - start + 1;
-    return Array.from({ length }, (_, i) => start + i - 1);
-  }
+  async function extractPdfPages() {
+    const fromPage = parseInt(document.getElementById("fromPage").value);
+    const toPage = parseInt(document.getElementById("toPage").value);
 
-  async function extractPdfPage(arrayBuff) {
-    const pdfSrcDoc = await PDFDocument.load(arrayBuff);
-    const pdfNewDoc = await PDFDocument.create();
-    const pages = await pdfNewDoc.copyPages(pdfSrcDoc, range(2, 4));
-    pages.forEach((page) => pdfNewDoc.addPage(page));
-    const newpdf = await pdfNewDoc.save();
-    return newpdf;
-  }
+    if (fromPage && toPage && fromPage <= toPage && toPage <= pdfPagesCount) {
+      const pdfSrcDoc = await PDFDocument.load(
+        await fetch(pdfFileData).then((res) => res.arrayBuffer())
+      );
+      const pdfNewDoc = await PDFDocument.create();
+      const pages = await pdfNewDoc.copyPages(
+        pdfSrcDoc,
+        Array.from(
+          { length: toPage - fromPage + 1 },
+          (_, i) => fromPage + i - 1
+        )
+      );
+      pages.forEach((page) => pdfNewDoc.addPage(page));
+      const newPdf = await pdfNewDoc.save();
+      const newPdfUrl = URL.createObjectURL(
+        new Blob([newPdf], { type: "application/pdf" })
+      );
 
+      // Replace original PDF with extracted pages
+      setPdfFileData(newPdfUrl);
+      setPdfPagesCount(pages.length);
+    } else {
+      alert("Invalid page range");
+    }
+  }
   // Execute when user select a file
   const onFileSelected = async (e) => {
     const fileList = e.target.files;
     if (fileList?.length > 0) {
       const pdfArrayBuffer = await readFileAsync(fileList[0]);
-      const newPdfDoc = await extractPdfPage(pdfArrayBuffer);
-      renderPdf(newPdfDoc);
+      await renderPdf(pdfArrayBuffer);
     }
   };
 
   return (
-    <>
-      <h1>Hello world</h1>
+    <div className="container my-5">
+      <h1>Extract Pages</h1>
       <input
         type="file"
+        className="form-control"
         id="file-selector"
         accept=".pdf"
         onChange={onFileSelected}
       />
-      <iframe
-        style={{ display: "block", width: "100vw", height: "90vh" }}
-        title="PdfFrame"
-        src={pdfFileData}
-        // frameborder="0"
-        type="application/pdf"
-      ></iframe>
-    </>
+      {pdfFileData && (
+        <div>
+          <label htmlFor="fromPage">Enter start page:</label>
+          <input type="number" id="fromPage" min="1" max={pdfPagesCount} />
+          <label htmlFor="toPage">Enter end page:</label>
+          <input type="number" id="toPage" min="1" max={pdfPagesCount} />
+          <button onClick={extractPdfPages}>Extract Pages</button>
+          <iframe
+            style={{ display: "block", width: "100vw", height: "90vh" }}
+            title="PdfFrame"
+            src={pdfFileData}
+            type="application/pdf"
+          ></iframe>
+        </div>
+      )}
+
+    </div>
   );
 }
